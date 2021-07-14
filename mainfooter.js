cbc167c7ee408b6974c83385952b1264ffb16f6f
Petula Pascall <petulapascall@gmail.com>
2021-07-13 17:03:52 -0700

    Update mainfooter.js

--- mainfooter.js
+++ mainfooter.js
@@ -73,6 +73,7 @@ let classname = document.getElementsByClassName("anim-confetti-button");
 for (let i = 0; i < classname.length; i++) {
 
 	classname[i].addEventListener("mouseover", animateCompleteButton, false);
+	classname[i].addEventListener("touchstart", animateCompleteButton, false);
 	
   
 }
