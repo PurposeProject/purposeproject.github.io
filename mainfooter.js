14bb909743bfbf4084866f21dcb388107f270f9f
Petula Pascall <petulapascall@gmail.com>
2021-07-14 12:48:14 -0700

    testing ui complete btn

--- mainfooter.js
+++ mainfooter.js
@@ -54,6 +54,48 @@ function downloadURI(uri, name) {
 
 // End Print
 
+// Complete Button Animation
+
+var animateCompleteButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+
+    document.getElementById("activityCompleteButton").style.display = "none";
+    document.getElementById("activityIsCompleteButton").style.display = "inline";
+  }, 700);
+
+  document.getElementById("activityCompleteButton").innerHTML = "ACTIVITY COMPLETED";
+  document.getElementById("activityCompleteButton").style.backgroundColor = "#2a3c63";
+  document.getElementById("activityCompleteButton").style.border = "none";
+  document.getElementById("activityCompleteButton").style.color = "white";
+};
+
+var animateCompleteButtonUndo = function (e) {
+  document.getElementById("activityCompleteButton").style.display = "inline";
+  document.getElementById("activityIsCompleteButton").style.display = "none";
+
+  document.getElementById("activityCompleteButton").innerHTML = "COMPLETE ACTIVITY";
+  document.getElementById("activityCompleteButton").style.backgroundColor = "white";
+  document.getElementById("activityCompleteButton").style.border = "2px solid #f38066";
+  document.getElementById("activityCompleteButton").style.color = "#f38066";
+};
+
+var classname = document.getElementsByClassName("anim-confetti-button");
+var classname2 = document.getElementsByClassName("activity-complete-button");
+
+for (var i = 0; i < classname.length; i++) {
+  classname[i].addEventListener("click", animateCompleteButton, false);
+
+  classname2[i].addEventListener("click", animateCompleteButtonUndo, false);
+}
+//////////////////////////////////////end test feature////////////////////////////
+/*
 
 // Complete Animated Button 
 
@@ -80,4 +122,4 @@ for (let i = 0; i < classname.length; i++) {
 	classname[i].addEventListener("ontouchmove", animateCompleteButton, false);
 	
   
-} 
+}  */
