d938239c9414099dd5167ff77dded9961ae50cea
Petula Pascall <petulapascall@gmail.com>
2021-08-29 14:32:38 -0700

    Update webflowbk_head08292021.js

--- webflowbk_head08292021.js
+++ webflowbk_head08292021.js
@@ -1,1 +1,391 @@
+<!-- Google Tag Manager -->
+ <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MP84TFP');</script>
+<!-- End Google Tag Manager --> 
+
+
+<link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
+<link rel="stylesheet" href="https://purposeproject.github.io/main.css" />
+
+<!-- Memberstack --> 
+<script src="https://api.memberstack.io/static/memberstack.js?webflow" data-memberstack-id="f68863429f9363b3b4721273c41232b7"> </script> 
+
+<!-- Memberstack - Redirect if JS is disabled-->
+
+<noscript>
+   <meta http-equiv="refresh" content="0; url=https://www.memberstack.io" />
+</noscript>
+
+
+<!--Memberstack session duration-->
+<script>  
+ms_settings = {  session:  976   } //hours
+</script>
+
+
+<!-- Donatebox Script-->
+<script type="text/javascript" defer src="https://donorbox.org/install-popup-button.js"></script>
+<script>window.DonorBox = { widgetLinkClassName: 'custom-dbox-popup' }</script>
+<!-- End Donatebox Script-->
+
+<!-- Mixpanel Tracking Code for www.purposeproject.org  -->
+<script type="text/javascript">
+(function(f,b){if(!b.__SV){var e,g,i,h;window.mixpanel=b;b._i=[];b.init=function(e,f,c){function g(a,d){var b=d.split(".");2==b.length&&(a=a[b[0]],d=b[1]);a[d]=function(){a.push([d].concat(Array.prototype.slice.call(arguments,0)))}}var a=b;"undefined"!==typeof c?a=b[c]=[]:c="mixpanel";a.people=a.people||[];a.toString=function(a){var d="mixpanel";"mixpanel"!==c&&(d+="."+c);a||(d+=" (stub)");return d};a.people.toString=function(){return a.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+for(h=0;h<i.length;h++)g(a,i[h]);var j="set set_once union unset remove delete".split(" ");a.get_group=function(){function b(c){d[c]=function(){call2_args=arguments;call2=[c].concat(Array.prototype.slice.call(call2_args,0));a.push([e,call2])}}for(var d={},e=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<j.length;c++)b(j[c]);return d};b._i.push([e,f,c])};b.__SV=1.2;e=f.createElement("script");e.type="text/javascript";e.async=!0;e.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?
+MIXPANEL_CUSTOM_LIB_URL:"file:"===f.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";g=f.getElementsByTagName("script")[0];g.parentNode.insertBefore(e,g)}})(document,window.mixpanel||[]);
+mixpanel.init("b548bb04bb3df4253a7f616019182f95");  
+</script>
+
+
+<!-- Hotjar Tracking Code for www.purposeproject.org  -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2537957,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+
+
+<!-- Load saved input text areas and input field from internal storage -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+</script>
+
+<script type="text/javascript">
+    
+    jQuery(window).on("load", function(){
+
+        $('input[type="text"], textarea').each(function(){    
+        let id = $(this).attr('id');
+        let value = localStorage.getItem(id);
+        
+        $(this).val(value);
+        
+    }); 
+    // resize textbox based on amt of content //
+    const tx = document.getElementsByTagName("textarea");
+		for (let i = 0; i < tx.length; i++) {
+  			tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  			tx[i].addEventListener("load", OnInput, false);
+		}
+
+		function OnInput() {
+  		this.style.height = "auto";
+  		this.style.height = (this.scrollHeight) + "px";
+		}   
+    
+    
+});
+
+
+
+</script>
+
+
+
+<style>
+  
+
+  
+ /* Browser resize for product page */
+
+
+
+  	@media only screen and (min-width: 1878px) and (max-width: 1919px) {
+ 
+       .div-block-23 {
+    height: 50ch;
+      
+  }
+  }
+      
+  @media only screen and (min-width: 1576px) and (max-width: 1877px) {
+ 
+       .div-block-23 {
+    height: 60ch;
+    
+  }    
+  }
+  
+    @media only screen and (min-width: 1406px) and (max-width: 1575px) {
+ 
+       .div-block-23 {
+    height: 70ch;
+    
+  }    
+  }
+  
+    @media only screen and (min-width: 1306px) and (max-width: 1405px) {
+ 
+       .div-block-23 {
+    height: 80ch;
+    
+  }    
+  }
+  
+  @media only screen and (min-width: 1100px) and (max-width: 1183px) {
+ 
+       .div-block-23 {
+    height: 100ch;
+        
+    
+  }    
+  }
+  
+  @media only screen and (min-width: 1043px) and (max-width: 1099px) {
+ 
+    .div-block-23 {
+    height: 110ch;
+     
+   	}    
+    
+    .paragraph-4, .paragraph-5, .paragraph-6 {
+    height: 55px;
+	}     
+   
+    
+  }
+ 
+@media only screen and (min-width: 1015px) and (max-width: 1042px) {
+ 
+    .div-block-23 {
+    height: 120ch;
+          
+  	}    
+  
+    .paragraph-4, .paragraph-5, .paragraph-6 {
+    height: 60px;
+	}     
+   
+  }
+  
+ @media only screen and (min-width: 992px) and (max-width: 1014px) {
+ 
+    .div-block-23 {
+    height: 130ch;
+         
+  	} 
+   
+   .paragraph-4, .paragraph-5, .paragraph-6 {
+    height: 60px;
+	}     
+   
+  } 
+  
+
+
+ 
+</style>
+
+<style>
+/* Fix fonts looking bold */
+  body{
+    -webkit-font-smoothing: antialiased; /* This needs to be set or some font faced fonts look bold on Mac in Chrome/Webkit based browsers. */
+    -moz-osx-font-smoothing: grayscale; /* Fixes font bold issue in Firefox version 25+ on Mac */
+  }
+
+  .error-message {
+    display: none;
+  }
+/* if this causes issues with other audio players, revisit and make custom tags */
+  .plyr--audio .plyr__control.plyr__tab-focus,
+  .plyr--audio .plyr__control:hover,
+  .plyr--audio .plyr__control[aria-expanded=true] {
+      background: none;
+  }
+  
+    .no-textarea-style {
+      border: none;
+      overflow: auto;
+      outline: none;
+      -webkit-box-shadow: none;
+      -moz-box-shadow: none;
+      box-shadow: none;
+	
+      resize: none; /*remove the resize handle on the bottom right*/
+  }
+
+  ul{
+      list-style: none; /* Remove default bullets */
+  }
+  ul li.body-main::before {
+      content: "\2022";  /* Add content: \2022 is the CSS Code/unicode for a bullet */
+      color: black; /* Change the color */
+      font-weight: bold; /* If you want it to be bold */
+      display: inline-block; /* Needed to add space between the bullet and the text */
+      width: 1em; /* Also needed for space (tweak if needed) */
+      margin-left: -1em; /* Also needed for space (tweak if needed) */
+  }
+  
+    textarea.w-input{
+    resize:vertical;
+    }
+  
+</style>
+
+  <!-- Prevent interactables from being dragged -->
+<style>
+  img {
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+  a {
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+}
+</style>
+
+<!-- Profile Styles -->
+<style>
+.profile-activity-card, .mix.card-curiosity.explore.profile-activity-card {
+  display: none;
+}
+</style>
+
+<!-- Quiz Styles -->
+<style>
+.quiz-button {
+  cursor: pointer;
+}
+
+.quiz-button.selected .quiz-button-text {
+  color: white;
+}
+
+.quiz-button.selected .quiz-button-header {
+  color: white;
+}
+
+#quiz-slide-1 .quiz-button.selected {
+  background-color: #657BB6; /* purple */
+}
+
+#quiz-slide-1 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+
+#quiz-slide-2 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-2 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+
+#quiz-slide-3 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-3 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+
+#quiz-slide-4 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-4 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+
+#quiz-slide-5 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-5 .quiz-button:hover {
+  border: 4px solid #657BB6; 
+}
+
+#quiz-slide-6 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-6 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+  
+#quiz-slide-7 .quiz-button.selected {
+  background-color: #657BB6;
+}
+
+#quiz-slide-7 .quiz-button:hover {
+  border: 4px solid #657BB6;
+}
+
+  
+#quiz-slide-2 .quiz-button.selected .quiz-button-text,
+#quiz-slide-6 .quiz-button.selected .quiz-button-text,
+#quiz-slide-7 .quiz-button.selected .quiz-button-text {
+  color: #2a3c63;
+}
+
+#quiz-slide-2 .quiz-button.selected .quiz-button-header,
+#quiz-slide-6 .quiz-button.selected .quiz-button-header,
+#quiz-slide-7 .quiz-button.selected .quiz-button-header {
+  color: #2a3c63;
+}
+
+
+
+@media screen and (max-width: 768px) {
+	.underline-nav.w--current {
+    	width: 100%;
+    }
+}
+
+</style>
+
+<style> 
+  .marketing-page .w-container { 
+    max-width: 1200px; 
+  } 
+
+  .marketing-page .body-main {
+  	font-family: 'IBM Plex Serif',sans-serif;
+  }
+  
+
+/* Browser resize for skill page for short laptop screen */
+
+
+
+  
+  	@media only screen and (min-width: 1250px) and (max-width: 1285px) and (min-height: 540px) and (max-height: 590px)  {
+ 
+      #arrow-skills {
+    display: flex;
+      
+  };
+      #video-skills {
+      
+      width: 70%;
+      height: 70%;  
+      
+      }
+      
+      
+  }
+     
+
+</style>
+
+
 
